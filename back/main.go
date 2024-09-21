d3b9964be6a30893681487ce988759515a940ed4
upprsk <lucassilveiraross@gmail.com>
2024-09-20 22:00:56 -0300

    feat: :sparkles: when a user is created, force the role to be viewer

--- back/main.go
+++ back/main.go
@@ -1,24 +1,36 @@
 package main
 
 import (
-    "log"
-    "os"
+	"log"
+	"os"
 
-    "github.com/pocketbase/pocketbase"
-    "github.com/pocketbase/pocketbase/apis"
-    "github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/models"
 )
 
 func main() {
-    app := pocketbase.New()
+	app := pocketbase.New()
 
-    // serves static files from the provided public dir (if exists)
-    app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-        e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
-        return nil
-    })
+	// serves static files from the provided public dir (if exists)
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		return nil
+	})
 
-    if err := app.Start(); err != nil {
-        log.Fatal(err)
-    }
+	app.OnRecordAfterCreateRequest("users").Add(func(e *core.RecordCreateEvent) error {
+		admin, _ := e.HttpContext.Get(apis.ContextAdminKey).(*models.Admin)
+		if admin != nil {
+			return nil // ignore for admins
+		}
+
+		e.Record.Set("role", "viewer")
+
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
