d3714019d61969f103f5e9dd57f07e8366006a94
upprsk <lucassilveiraross@gmail.com>
2024-09-26 23:03:28 -0300

    feat: :sparkles: added back button for mobile

--- front/src/lib/components/page-grid.svelte
+++ front/src/lib/components/page-grid.svelte
@@ -1,6 +1,14 @@
 <div class="grid w-full grid-cols-1 p-5 sm:grid-cols-2">
-  <div class="breadcrumbs hidden text-sm sm:block" class:col-span-2={!$$slots.actions}>
-    <slot name="breadcrumbs" />
+  <div>
+    <div class="breadcrumbs hidden text-sm sm:block" class:col-span-2={!$$slots.actions}>
+      <slot name="breadcrumbs" />
+    </div>
+
+    <div>
+      <button on:click={() => history.back()} type="button" class="btn btn-error btn-xs sm:hidden">
+        voltar
+      </button>
+    </div>
   </div>
 
   {#if $$slots.actions}
