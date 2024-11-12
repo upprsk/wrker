681ec8adceacf8ec4386996c7e1f39e7c8b45405
upprsk <lucassilveiraross@gmail.com>
2024-11-12 09:24:08 -0300

    test: :test_tube: added more tests

--- back/main_test.go
+++ back/main_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"testing"
@@ -416,6 +419,367 @@ func TestGetPollQuestions(t *testing.T) {
 			},
 			TestAppFactory: setupTestApp,
 		},
+	}
+
+	for _, scenario := range scenarios {
+		scenario.Test(t)
+	}
+}
+
+func TestGetPollAnswers(t *testing.T) {
+	bobToken, err := generateRecordToken("users", "bob@example.com")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tomToken, err := generateRecordToken("users", "tom@example.com")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	daveToken, err := generateRecordToken("users", "dave@example.com")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	editorToken, err := generateRecordToken("users", "admin@example.com")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	adminToken, err := generateAdminToken("the.admin@example.com")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// setup the test ApiScenario app instance
+	setupTestApp := func(t *testing.T) *tests.TestApp {
+		testApp, err := tests.NewTestApp(testDataDir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// no need to cleanup since scenario.Test() will do that for us
+		// defer testApp.Cleanup()
+
+		bindAppHooks(testApp)
+
+		return testApp
+	}
+
+	createFilterParams := func(pollId string) string {
+		values := url.Values{}
+		values.Add("filter", fmt.Sprintf("question.poll.id='%s'", pollId))
+
+		return values.Encode()
+	}
+
+	scenarios := []tests.ApiScenario{
+		{
+			Name:            "try as guest (aka. no Authorization header)",
+			Method:          http.MethodGet,
+			Url:             "/api/collections/pollAnswers/records?" + createFilterParams("9keexq337xq9au2"),
+			ExpectedStatus:  200,
+			ExpectedEvents:  map[string]int{"OnRecordsListRequest": 1},
+			ExpectedContent: []string{"\"page\":1", "\"totalItems\":0", "\"items\":[]"},
+			TestAppFactory:  setupTestApp,
+		},
+		{
+			Name:            "try as dave",
+			Method:          http.MethodGet,
+			Url:             "/api/collections/pollAnswers/records?" + createFilterParams("9keexq337xq9au2"),
+			ExpectedStatus:  200,
+			RequestHeaders:  map[string]string{"Authorization": daveToken},
+			ExpectedEvents:  map[string]int{"OnRecordsListRequest": 1},
+			ExpectedContent: []string{"\"page\":1", "\"totalItems\":0", "\"items\":[]"},
+			TestAppFactory:  setupTestApp,
+		},
+		{
+			Name:           "try as bob",
+			Method:         http.MethodGet,
+			Url:            "/api/collections/pollAnswers/records?" + createFilterParams("9keexq337xq9au2"),
+			ExpectedStatus: 200,
+			RequestHeaders: map[string]string{"Authorization": bobToken},
+			ExpectedEvents: map[string]int{"OnRecordsListRequest": 1},
+			ExpectedContent: []string{
+				"\"page\":1",
+				"\"totalItems\":3",
+				"\"answer\":\"preto\"",
+				"\"answer\":2",
+				"\"answer\":\"verde\"",
+			},
+			TestAppFactory: setupTestApp,
+		},
+		{
+			Name:           "try as tom",
+			Method:         http.MethodGet,
+			Url:            "/api/collections/pollAnswers/records?" + createFilterParams("9keexq337xq9au2"),
+			ExpectedStatus: 200,
+			RequestHeaders: map[string]string{"Authorization": tomToken},
+			ExpectedEvents: map[string]int{"OnRecordsListRequest": 1},
+			ExpectedContent: []string{
+				"\"page\":1",
+				"\"totalItems\":3",
+				"\"answer\":\"preto\"",
+				"\"answer\":\"branco\"",
+				"\"answer\":\"2\"",
+			},
+			TestAppFactory: setupTestApp,
+		},
+		{
+			Name:           "try as editor",
+			Method:         http.MethodGet,
+			Url:            "/api/collections/pollAnswers/records?" + createFilterParams("9keexq337xq9au2"),
+			ExpectedStatus: 200,
+			RequestHeaders: map[string]string{"Authorization": editorToken},
+			ExpectedEvents: map[string]int{"OnRecordsListRequest": 1},
+			ExpectedContent: []string{
+				"\"page\":1",
+				"\"totalItems\":6",
+				"\"answer\":\"preto\"",
+				"\"answer\":\"branco\"",
+				"\"answer\":\"2\"",
+				"\"answer\":2",
+			},
+			TestAppFactory: setupTestApp,
+		},
+		{
+			Name:           "try as admin",
+			Method:         http.MethodGet,
+			Url:            "/api/collections/pollAnswers/records?" + createFilterParams("9keexq337xq9au2"),
+			ExpectedStatus: 200,
+			RequestHeaders: map[string]string{"Authorization": adminToken},
+			ExpectedEvents: map[string]int{"OnRecordsListRequest": 1},
+			ExpectedContent: []string{
+				"\"page\":1",
+				"\"totalItems\":6",
+				"\"answer\":\"preto\"",
+				"\"answer\":\"branco\"",
+				"\"answer\":\"2\"",
+				"\"answer\":2",
+			},
+			TestAppFactory: setupTestApp,
+		},
+	}
+
+	for _, scenario := range scenarios {
+		scenario.Test(t)
+	}
+}
+
+func TestCreatePoll(t *testing.T) {
+	/*
+		bobToken, err := generateRecordToken("users", "bob@example.com")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		tomToken, err := generateRecordToken("users", "tom@example.com")
+		if err != nil {
+			t.Fatal(err)
+		}
+	*/
+
+	daveToken, err := generateRecordToken("users", "dave@example.com")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	editorToken, err := generateRecordToken("users", "admin@example.com")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	/*
+		adminToken, err := generateAdminToken("the.admin@example.com")
+		if err != nil {
+			t.Fatal(err)
+		}
+	*/
+
+	// setup the test ApiScenario app instance
+	setupTestApp := func(t *testing.T) *tests.TestApp {
+		testApp, err := tests.NewTestApp(testDataDir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// no need to cleanup since scenario.Test() will do that for us
+		// defer testApp.Cleanup()
+
+		bindAppHooks(testApp)
+
+		return testApp
+	}
+
+	createJson := func(t *testing.T, data map[string]any) io.Reader {
+		var buf bytes.Buffer
+		if err := json.NewEncoder(&buf).Encode(data); err != nil {
+			t.Fatal(err)
+		}
+
+		return &buf
+	}
+
+	scenarios := []tests.ApiScenario{
+		{
+			Name:            "try as guest (aka. no Authorization header)",
+			Method:          http.MethodPost,
+			Url:             "/api/collections/polls/records",
+			ExpectedStatus:  400,
+			ExpectedContent: []string{"\"data\":{}"},
+			TestAppFactory:  setupTestApp,
+		},
+		{
+			Name:   "try as guest with body",
+			Method: http.MethodPost,
+			Url:    "/api/collections/polls/records",
+			Body: createJson(t, map[string]any{
+				"name":        "hello!",
+				"owner":       "nozq23vjwel6egq",
+				"description": "The description",
+			}),
+			ExpectedStatus:  400,
+			ExpectedContent: []string{"\"data\":{}"},
+			TestAppFactory:  setupTestApp,
+		},
+		{
+			Name:           "try as guest with missing fields",
+			Method:         http.MethodPost,
+			Url:            "/api/collections/polls/records",
+			Body:           createJson(t, map[string]any{}),
+			ExpectedStatus: 400,
+			ExpectedContent: []string{
+				"\"name\":{\"code\":\"validation_required\",\"message\":\"Missing required value.\"}",
+				"\"owner\":{\"code\":\"validation_required\",\"message\":\"Missing required value.\"}",
+				"\"description\":{\"code\":\"validation_required\",\"message\":\"Missing required value.\"}",
+			},
+			TestAppFactory: setupTestApp,
+		},
+		{
+			Name:            "try as dave",
+			Method:          http.MethodPost,
+			Url:             "/api/collections/polls/records",
+			ExpectedStatus:  400,
+			RequestHeaders:  map[string]string{"Authorization": daveToken},
+			ExpectedContent: []string{"\"data\":{}"},
+			TestAppFactory:  setupTestApp,
+		},
+		{
+			Name:           "try as dave with body",
+			Method:         http.MethodPost,
+			Url:            "/api/collections/polls/records",
+			ExpectedStatus: 400,
+			RequestHeaders: map[string]string{"Authorization": daveToken},
+			Body: createJson(t, map[string]any{
+				"name":        "hello!",
+				"owner":       "nozq23vjwel6egq",
+				"description": "The description",
+			}),
+			ExpectedContent: []string{"\"data\":{}"},
+			TestAppFactory:  setupTestApp,
+		},
+		{
+			Name:           "try as dave with missing fields",
+			Method:         http.MethodPost,
+			Url:            "/api/collections/polls/records",
+			ExpectedStatus: 400,
+			RequestHeaders: map[string]string{"Authorization": daveToken},
+			Body:           createJson(t, map[string]any{}),
+			ExpectedContent: []string{
+				"\"name\":{\"code\":\"validation_required\",\"message\":\"Missing required value.\"}",
+				"\"owner\":{\"code\":\"validation_required\",\"message\":\"Missing required value.\"}",
+				"\"description\":{\"code\":\"validation_required\",\"message\":\"Missing required value.\"}",
+			},
+			TestAppFactory: setupTestApp,
+		},
+		{
+			Name:            "try as editor with no body",
+			Method:          http.MethodPost,
+			Url:             "/api/collections/polls/records",
+			ExpectedStatus:  400,
+			RequestHeaders:  map[string]string{"Authorization": editorToken},
+			ExpectedContent: []string{"\"data\":{}"},
+			TestAppFactory:  setupTestApp,
+		},
+		{
+			Name:           "try as editor with missing fields",
+			Method:         http.MethodPost,
+			Url:            "/api/collections/polls/records",
+			ExpectedStatus: 400,
+			RequestHeaders: map[string]string{"Authorization": editorToken},
+			Body:           createJson(t, map[string]any{}),
+			ExpectedContent: []string{
+				"\"name\":{\"code\":\"validation_required\",\"message\":\"Missing required value.\"}",
+				"\"owner\":{\"code\":\"validation_required\",\"message\":\"Missing required value.\"}",
+				"\"description\":{\"code\":\"validation_required\",\"message\":\"Missing required value.\"}",
+			},
+			TestAppFactory: setupTestApp,
+		},
+		{
+			Name:           "try as editor",
+			Method:         http.MethodPost,
+			Url:            "/api/collections/polls/records",
+			ExpectedStatus: 200,
+			RequestHeaders: map[string]string{"Authorization": editorToken},
+			ExpectedEvents: map[string]int{
+				"OnModelBeforeCreate":         1,
+				"OnModelAfterCreate":          1,
+				"OnRecordAfterCreateRequest":  1,
+				"OnRecordBeforeCreateRequest": 1,
+			},
+			Body: createJson(t, map[string]any{
+				"name":        "hello!",
+				"owner":       "r34ryr15narwfto",
+				"description": "The description",
+			}),
+			ExpectedContent: []string{
+				"\"anonymous\":false",
+				"\"audience\":[]",
+				"\"closingDate\":\"\"",
+				"\"description\":\"The description\"",
+				"\"name\":\"hello!\"",
+				"\"open\":false",
+				"\"owner\":\"r34ryr15narwfto\"",
+			},
+			TestAppFactory: setupTestApp,
+		},
+		/*
+			{
+				Name:           "try as editor",
+				Method:         http.MethodGet,
+				Url:            "/api/collections/pollAnswers/records?" + createFilterParams("9keexq337xq9au2"),
+				ExpectedStatus: 200,
+				RequestHeaders: map[string]string{"Authorization": editorToken},
+				ExpectedEvents: map[string]int{"OnRecordsListRequest": 1},
+				ExpectedContent: []string{
+					"\"page\":1",
+					"\"totalItems\":6",
+					"\"answer\":\"preto\"",
+					"\"answer\":\"branco\"",
+					"\"answer\":\"2\"",
+					"\"answer\":2",
+				},
+				TestAppFactory: setupTestApp,
+			},
+		*/
+		/*
+			{
+				Name:           "try as admin",
+				Method:         http.MethodGet,
+				Url:            "/api/collections/pollAnswers/records?" + createFilterParams("9keexq337xq9au2"),
+				ExpectedStatus: 200,
+				RequestHeaders: map[string]string{"Authorization": adminToken},
+				ExpectedEvents: map[string]int{"OnRecordsListRequest": 1},
+				ExpectedContent: []string{
+					"\"page\":1",
+					"\"totalItems\":6",
+					"\"answer\":\"preto\"",
+					"\"answer\":\"branco\"",
+					"\"answer\":\"2\"",
+					"\"answer\":2",
+				},
+				TestAppFactory: setupTestApp,
+			},
+		*/
 	}
 
 	for _, scenario := range scenarios {
