fd04199b828115aff8ebae1ccc0715c760693675
LucaFelipe <luca.felipe@ges.inatel.br>
2024-11-22 06:21:17 -0300

    atualizado o config do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,37 +1,14 @@
-import { defineConfig } from 'cypress';
+const { defineConfig } = require('cypress');
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
-    supportFile: '../cypress/support/e2e.js',
-    baseUrl: 'http://localhost:5173', // Atualize para a porta correta
-    setupNodeEvents(on, config) {
-      // Evento para registrar falhas com capturas de tela
-      on('after:screenshot', (details) => {
-        console.log('Screenshot taken:', details.path);
-      });
-
-      // Evento para fazer logging de erros de forma detalhada
-      on('task', {
-        log(message) {
-          console.log(message);
-          return null;
-        },
-      });
-
-      // Evento para capturar capturas de tela e gerar relatórios de falhas
-      on('after:spec', (spec, results) => {
-        if (results && results.stats.failures > 0) {
-          const screenshots = results.screenshots.map(s => s.path);
-          console.log(`Test failed in ${spec.name}. Screenshots:`, screenshots);
-        }
-      });
-
-      // Outros eventos de uso comum podem ser adicionados aqui
-      return config;
+    baseUrl: 'http://localhost:5173',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports', // Diretório onde o relatório será salvo
+      overwrite: false,            // Não sobrescreve relatórios anteriores
+      html: true,                  // Gera relatório em HTML
+      json: true,                  // Gera relatório em JSON
     },
-    // Configurações adicionais de Cypress
-    defaultCommandTimeout: 8000, // Tempo limite para comandos
-    retries: 2,                  // Número de tentativas de reexecução em caso de falhas
-    video: true,                 // Gravação de vídeos dos testes
   },
 });
